4758a5029d25e869a6b33d355ac211b9607a6922
m0d2r <mineceafjozikrorokcz@gmail.com>
2025-03-16 03:14:53 +0100

    LandawasOS V0.01

---
+++ readme.md
@@ -0,0 +1,10 @@
+# LandawasOS
+LandawasOS is an operating system for cc: tweaks
+
+# how to install
+1. open your game folder
+2. open "saves" folder
+3. open your world folder
+4. open computercraft folder
+5. open disk/ or rom/ (on your preference)
+6. place "s
---
+++ startup.lua
@@ -0,0 +1,12 @@
+term.clear()
+term.setCursorPos(1,1)
+disk.setLabel("disk", "Landawas Operating System")
+
+term.setTextColour(colors.lime)
+term.write("LandawasOS")
+term.setTextColour(colors.white)
+print(" By StormPatrik")
+
+print("Thank you for chosing LandawasOS")
+print("Beta: V0.01")
+textutils.slowPrint("---------------------------------------------------")
